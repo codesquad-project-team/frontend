a62c8b1ceddf0a6feea9edfaa939b504d5065500
Hwarang Lee <hwrng2@gmail.com>
2019-09-26 17:55:19 +0900

    [1] 개발 환경 설정 (#8)
    * setting: 필요 모듈 설치
    
    * setting: 웹팩, 바벨 설정
    
    * setting: 초기 마크업 및 스타일 파일 생성

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [
+    [
+      '@babel/preset-env'
+    ],
+    [
+      '@babel/preset-react'
+    ]
+  ]
+
+  const plugins = [
+    "@babel/plugin-transform-runtime"
+  ]
+
+  return {
+    presets,
+    plugins
+  }
+}
