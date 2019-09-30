324028395aa3d7962cd1a457edc7b8c5677f4086
AllenK <revenoir7@gmail.com>
2019-10-01 00:29:11 +0900

    feat: DetailPage 컴포넌트 작성을 위한 초기 파일 생성
    - Root.jsx : 리액트 라우터를 활용하여 pages컴포넌트들을 렌더링할 컴포넌트

---
+++ src/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DetailPage from '../src/pages/DetailPage/DetailPage';
+
+const Root = () => {
+  return (
+    <>
+      <DetailPage />
+    </>
+  );
+};
+
+export default Root;
---
+++ src/pages/DetailPage/DetailPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DetailPage.scss';
+
+const DetailPage = () => {
+  return (
+    <div className="DetailPage">
+      <h1>This is Detail Page</h1>
+    </div>
+  );
+};
+
+export default DetailPage;
