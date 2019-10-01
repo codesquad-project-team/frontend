1004ab3a751d7b2486db2a0da6b80460f775a585
AllenK <revenoir7@gmail.com>
2019-10-01 20:12:25 +0900

    feat: 검색창 추가
    - useInput 커스텀 훅 활용하여 input의 상태를 관리

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './Header.scss';
+import useInput from '../../hooks/useInput';
 import ProfileImage from '../ProfileImage/ProfileImage';
 
 const Header = () => {
+  const [inputValue, handleChange, restore] = useInput();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    restore('searchBar');
+  };
+
   return (
     <div className="Header">
       <h1>Logo</h1>
-      <input />
+      <form onSubmit={handleSubmit}>
+        <input
+          name="searchBar"
+          value={inputValue.searchBar}
+          onChange={handleChange}
+        />
+      </form>
       <ProfileImage small />
     </div>
   );
