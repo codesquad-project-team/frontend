bf8f07334d1ac82389ca04b3d02cf7c58333fea1
AllenK <revenoir7@gmail.com>
2019-10-01 20:05:50 +0900

    feat: Header 컴포넌트 생성

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.scss';
+import ProfileImage from '../ProfileImage/ProfileImage';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1>Logo</h1>
+      <input />
+      <ProfileImage small />
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './DetailPage.scss';
+import Header from '../../components/Header/Header';
 
 const DetailPage = () => {
   return (
     <div className="DetailPage">
+      <Header />
       <h1>This is Detail Page</h1>
     </div>
   );
