daa2c062d621bf591fb5d75313509eb2ed0381b9
leehwarang <hwrng2@gmail.com>
2019-10-02 15:00:01 +0900

    feat: DetailPost 추가
    - post title
    - post content (writer-immg, writer-name, post-desc)

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import DetailPost from '../../components/DetailPost/DetailPost';
 import './DetailPage.scss';
 
 const DetailPage = () => {
   return (
     <div className="DetailPage">
       <h1>This is Detail Page</h1>
+      <DetailPost />
     </div>
   );
 };
