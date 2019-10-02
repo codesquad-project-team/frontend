a5d7542d00d612a655609e0c531efdaa0b595bd9
leehwarang <hwrng2@gmail.com>
2019-10-02 17:36:27 +0900

    feat: LocationCarousel 컴포넌트 추가

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import Root from './Root';
 import './stylesheets/base.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 
 render(<Root />, document.querySelector('#root'));
