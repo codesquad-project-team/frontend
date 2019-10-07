02862aae470d9bd1f4bc80b1b01ee1fe37112f38
leehwarang <hwrng2@gmail.com>
2019-10-07 15:37:47 +0900

    feat&style: RelatedPost에 데이터 개수에 따라 유동적인 UI 구성
    - dummy data 추가
    - 데이터 개수에 따라 캐러셀의 아이템을 동적으로 생성
    - overflow:hidden 속성으로 5개 이상 아이템이라면 보이지 않게 하는 기능

--- src/components/RelatedPost/RelatedPost.jsx
+++ src/components/RelatedPost/RelatedPost.jsx
@@ -1,59 +1,112 @@
 import React, { useState } from 'react';
 import './RelatedPost.scss';
 import ProfileImage from '../ProfileImage/ProfileImage';
+import parse from 'html-react-parser';
 
 const RelatedPost = () => {
-  return (
-    <>
-      <div class="related-post">
-        <hr></hr>
-        <h2 class="related-post-header">이 장소를 방문한 사람들</h2>
+  const [carouselVisible, setCarouselVisible] = useState(true);
 
+  const [datas, setDatas] = useState([
+    {
+      profileUrl: 'https://avatars0.githubusercontent.com/u/18614517?s=460&v=4',
+      titleCompanion: '친구랑',
+      titleActivity: '코딩하기',
+      postId: 1
+    },
+    {
+      profileUrl: 'https://avatars0.githubusercontent.com/u/18614517?s=460&v=4',
+      titleCompanion: '친구랑',
+      titleActivity: '코딩하기',
+      postId: 1
+    },
+    {
+      profileUrl: 'https://avatars0.githubusercontent.com/u/18614517?s=460&v=4',
+      titleCompanion: '친구랑',
+      titleActivity: '코딩하기',
+      postId: 1
+    },
+    {
+      profileUrl: 'https://avatars0.githubusercontent.com/u/18614517?s=460&v=4',
+      titleCompanion: '친구랑',
+      titleActivity: '코딩하기',
+      postId: 1
+    },
+    {
+      profileUrl: 'https://avatars0.githubusercontent.com/u/18614517?s=460&v=4',
+      titleCompanion: '친구랑',
+      titleActivity: '코딩하기',
+      postId: 1
+    },
+    {
+      profileUrl: 'https://avatars0.githubusercontent.com/u/18614517?s=460&v=4',
+      titleCompanion: '친구랑',
+      titleActivity: '코딩하기',
+      postId: 1
+    },
+    {
+      profileUrl: 'https://avatars0.githubusercontent.com/u/18614517?s=460&v=4',
+      titleCompanion: '친구랑',
+      titleActivity: '코딩하기',
+      postId: 1
+    },
+    {
+      profileUrl: 'https://avatars0.githubusercontent.com/u/18614517?s=460&v=4',
+      titleCompanion: '친구랑',
+      titleActivity: '코딩하기',
+      postId: 1
+    }
+  ]);
+
+  const makeCarouselItem = () => {
+    const items = [...datas];
+
+    const result = items.reduce((acc, cur) => {
+      acc += `
+          <div class="related-post-carousel-item">
+            <h3 class="related-post-comment">
+            <ProfileImage medium></ProfileImage>
+              ${cur.titleCompanion}<br />
+              ${cur.titleActivity}
+            </h3>
+          </div>
+      `;
+      return acc;
+    }, '');
+
+    return parse(result, {
+      replace: domNode => {
+        if (domNode.name === 'profileimage') {
+          return <ProfileImage medium></ProfileImage>;
+        }
+      }
+    });
+  };
+
+  const makeCarouselJsx = () => {
+    const len = datas.length;
+
+    if (!len) {
+      return (
         <div class="related-post-carousel-wrap">
-          <div class="related-post-carousel">
-            <div class="related-post-carousel-item">
-              <ProfileImage medium></ProfileImage>
-              <h3 class="related-post-comment">
-                누구랑 <br />
-                ~하기
-              </h3>
-            </div>
-            <div class="related-post-carousel-item">
-              <ProfileImage medium></ProfileImage>
-              <h3 class="related-post-comment">
-                누구랑 <br />
-                ~하기
-              </h3>
-            </div>
-            <div class="related-post-carousel-item">
-              <ProfileImage medium></ProfileImage>
-              <h3 class="related-post-comment">
-                누구랑 <br />
-                ~하기
-              </h3>
-            </div>
-            <div class="related-post-carousel-item">
-              <ProfileImage medium></ProfileImage>
-              <h3 class="related-post-comment">
-                누구랑 <br />
-                ~하기
-              </h3>
-            </div>
-            <div class="related-post-carousel-item">
-              <ProfileImage medium></ProfileImage>
-              <h3 class="related-post-comment">
-                누구랑 <br />
-                ~하기
-              </h3>
-            </div>
-          </div>
-          <div class="related-post-carousel-btns">
-            <button class="carousel-btns-common prev-btn">&lt;</button>
-            <button class="carousel-btns-common next-btn">&gt;</button>
-          </div>
+          <h3>아직 이 장소를 방문한 다른 사람이 없네요. 🥺</h3>;
         </div>
+      );
+    } else {
+      return <div class="related-post-carousel-wrap">{makeCarouselItem()}</div>;
+    }
+  };
+
+  return (
+    <div class="related-post">
+      <hr></hr>
+      <h2 class="related-post-header">이 장소를 방문한 사람들</h2>
+
+      <div class="related-post-carousel">{makeCarouselJsx()}</div>
+      <div class="related-post-carousel-btns">
+        <button class="carousel-btns-common prev-btn">&lt;</button>
+        <button class="carousel-btns-common next-btn">&gt;</button>
       </div>
-    </>
+    </div>
   );
 };
 
