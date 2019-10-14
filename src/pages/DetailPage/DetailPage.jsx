5e6cc6d4194a7359c58d0cd6eb02e460c045d6eb
Heungsoo, Kim <revenoir7@gmail.com>
2019-10-14 09:53:25 +0900

    [2-1] 장소 디테일 페이지 - 헤더 구현 (#9)
    * feat: Header 컴포넌트 생성
    
    * feat: 검색창 추가
    
    - useInput 커스텀 훅 활용하여 input의 상태를 관리
    
    * style: header width를 100%로 수정
    
    * style: 검색창에 돋보기 아이콘 추가 및 스타일 조정
    
    * feat: 로그인, 회원가입 버튼 추가 및 헤더 레이아웃 flex로 정리
    
    * feat: 로그인 상태에 따라 헤더 오른쪽상단 렌더링 바뀌는 코드 추가
    
    - 임시 전역 변수로 작성
    - 로그인 기능 구현시 수정 예정

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -3,10 +3,12 @@ import DetailPost from '../../components/DetailPost/DetailPost';
 import LocationCarousel from '../../components/LocationCarousel/LocationCarousel';
 import RelatedPost from '../../components/RelatedPost/RelatedPost';
 import './DetailPage.scss';
+import Header from '../../components/Header/Header';
 
 const DetailPage = () => {
   return (
     <div className="DetailPage">
+      <Header />
       <h1>This is Detail Page</h1>
       <LocationCarousel></LocationCarousel>
       <DetailPost />
