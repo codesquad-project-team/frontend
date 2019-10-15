6bd14be09881f024faa74d66081c35984ee4905e
leehwarang <hwrng2@gmail.com>
2019-10-15 18:23:42 +0900

    delete: LoginPage 컴포넌트 삭제
    - login 기능은 페이지가 아니라 모달창으로 기획 수정하여 페이지 컴포넌트 삭제

--- src/Root.jsx
+++ src/Root.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage';
 import DetailPage from '../src/pages/DetailPage/DetailPage';
-import LoginPage from '../src/pages/LoginPage/LoginPage';
 
 const Root = () => {
   return (
@@ -12,7 +11,6 @@ const Root = () => {
         <Switch>
           <Route exact path="/" render={() => <MainPage />}></Route>
           <Route path="/post" render={() => <DetailPage />}></Route>
-          <Route path="/login" render={() => <LoginPage />}></Route>
         </Switch>
       </Router>
     </>
