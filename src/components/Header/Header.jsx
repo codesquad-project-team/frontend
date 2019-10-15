987a7dca6b450d5ada9c9a208b6afd713c0a2a4b
leehwarang <hwrng2@gmail.com>
2019-10-15 18:26:24 +0900

    feat: Header에서 회원가입 버튼 클릭했을 때 모달창 뜨도록 기능 구현
    - 사용자가 로그인하지 않았고, 회원가입 버튼 클릭했을 때 CommonModal 컴포넌트가 렌더링

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
 import useInput from '../../hooks/useInput';
 import ProfileImage from '../ProfileImage/ProfileImage';
 import CommonBtn from './CommonBtn';
+import CommonModal from '../CommonModal/CommonModal';
 
-const isLoggedIn = true; //TODO: 로그인 기능 구현시 수정
+const isLoggedIn = false; //TODO: 로그인 기능 구현시 수정
 
 const Header = () => {
   const [inputValue, handleChange, restore] = useInput();
+  const [clickedSignup, setClickedSignup] = useState(false);
 
   const handleSubmit = e => {
     e.preventDefault();
     restore('searchBar');
+  };
+
+  const handleSignup = () => {
+    clickedSignup ? setClickedSignup(false) : setClickedSignup(true);
   };
 
   return (
@@ -36,11 +42,16 @@ const Header = () => {
           <ProfileImage small />
         ) : (
           <>
-            <CommonBtn>로그인</CommonBtn>
-            <CommonBtn>회원가입</CommonBtn>
+            <CommonBtn className="login-btn">로그인</CommonBtn>
+            <CommonBtn className="signup-btn" onClick={handleSignup}>
+              회원가입
+            </CommonBtn>
           </>
         )}
       </div>
+      {!isLoggedIn && clickedSignup && (
+        <CommonModal signUpHandler={handleSignup}></CommonModal>
+      )}
     </div>
   );
 };
