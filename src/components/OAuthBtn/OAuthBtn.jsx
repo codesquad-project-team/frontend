e6316a3af8b5d63cbdff77cc5942ec4d1c9eb79a
leehwarang <hwrng2@gmail.com>
2019-10-17 16:27:17 +0900

    feat: OAuth 컴포넌트가 signin, signup의 모달창에서 재사용되도록 구현

--- src/components/OAuthBtn/OAuthBtn.jsx
+++ src/components/OAuthBtn/OAuthBtn.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './OAuthBtn.scss';
 
-const OAuthBtn = ({ company, color, imgUrl }) => {
+const OAuthBtn = ({ company, color, imgUrl, msg }) => {
   return (
-    <div className="OAuth-btn" style={{ 'background-color': color }}>
+    <div className="OAuth-btn" style={{ backgroundColor: color }}>
       <a className="OAuth-btn-company" href="">
-        {company} 계정으로 가입하기
+        {company} 계정으로 {msg}
       </a>
     </div>
   );
