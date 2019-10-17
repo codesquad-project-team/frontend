165b1ac1d1e3d574fad5fd96d479f8d2e5445685
leehwarang <hwrng2@gmail.com>
2019-10-17 16:26:27 +0900

    feat: CommonModal 컴포넌트가 signin, signup의 모달창에서 재사용되도록 구현

--- src/components/CommonModal/CommonModal.jsx
+++ src/components/CommonModal/CommonModal.jsx
@@ -2,8 +2,31 @@ import React, { useState, useEffect } from 'react';
 import './CommonModal.scss';
 import OAuthBtn from '../OAuthBtn/OAuthBtn';
 
-const CommonModal = ({ signUpHandler }) => {
+const CommonModal = ({ clickHandler, target }) => {
+  const [content, setContent] = useState({});
+
+  const makeContent = parent => {
+    console.log(parent);
+    if (parent === `signin`) {
+      setContent({
+        title: '시작하기',
+        desc: '로그인 하는 내용과 관련된 말 블라블라블라',
+        reminderMsg: '아직 계정이 없으신가요?',
+        hyperlinkMsg: '가입하기'
+      });
+    } else {
+      setContent({
+        title: '가입하기',
+        desc:
+          '이 곳에서 당신의 취향을 찾으세요. 그리고 현재의 감정 상태에 따라 지금 할 일을 정해보세요.',
+        reminderMsg: '이미 계정이 있으신가요?',
+        hyperlinkMsg: '로그인하기'
+      });
+    }
+  };
+
   useEffect(() => {
+    makeContent(target);
     document.body.style.overflow = 'hidden';
     return () => (document.body.style.overflow = 'unset');
   }, []);
@@ -12,20 +35,34 @@ const CommonModal = ({ signUpHandler }) => {
     <>
       <div className="common-modal-overlay" />
       <div className="common-modal">
-        <button className="common-modal-btn" onClick={signUpHandler}>
+        <button className="common-modal-btn" onClick={clickHandler}>
           닫기
         </button>
         <div className="common-modal-content">
-          <h1>서비스 시작하기</h1>
-          <p>이 곳에서 당신의 취향을 찾으세요.</p>
-          <p>그리고 현재의 감정 상태에 따라 지금 할 일을 정해보세요.</p>
+          <h1>서비스 {content.title}</h1>
+          <p>{content.desc}</p>
           <div className="common-modal-content-oauth">
-            <OAuthBtn company="구글" color="#DB4437" imgUrl=""></OAuthBtn>
-            <OAuthBtn company="페이스북" color="#4267B2" imgUrl=""></OAuthBtn>
-            <OAuthBtn company="인스타그램" color="#FCAF45" imgUrl=""></OAuthBtn>
+            <OAuthBtn
+              company="구글"
+              color="#DB4437"
+              msg={content.title}
+              imgUrl=""
+            ></OAuthBtn>
+            <OAuthBtn
+              company="페이스북"
+              color="#4267B2"
+              msg={content.title}
+              imgUrl=""
+            ></OAuthBtn>
+            <OAuthBtn
+              company="인스타그램"
+              color="#FCAF45"
+              msg={content.title}
+              imgUrl=""
+            ></OAuthBtn>
           </div>
           <p>
-            이미 계정이 있으신가요? <a herf="">로그인하기</a>
+            {content.reminderMsg} <a herf="">{content.hyperlinkMsg}</a>
           </p>
         </div>
       </div>
