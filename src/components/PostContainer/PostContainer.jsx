937330d35c2a0ed359d1115a5c0843a3564e7503
AllenK <revenoir7@gmail.com>
2019-10-18 13:52:29 +0900

    feat: post item눌렀을 때 detail page로 라우팅하는 기능 추가

--- src/components/PostContainer/PostContainer.jsx
+++ src/components/PostContainer/PostContainer.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './PostContainer.scss';
+import { Link } from 'react-router-dom';
 import PostItem from '../PostItem/PostItem';
 
 const PostContainer = ({ items, header }) => {
   const postItems = (items || []).map(e => (
-    <PostItem
+    <Link
+      to={`/post/${e.postId}`}
       key={e.postId || Math.floor(Math.random() * 10000)}
-      headerOn={header}
-      {...e}
-    ></PostItem>
+    >
+      <PostItem headerOn={header} {...e}></PostItem>
+    </Link>
   ));
   return (
     <div className="post-container-wrapper">
