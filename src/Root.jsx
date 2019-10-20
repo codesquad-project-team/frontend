8c12bec3dc18025bd29cece9694b8ca6b0a103c9
Hwarang Lee <hwrng2@gmail.com>
2019-10-20 19:43:35 +0900

    [6] 회원 정보 수정 페이지 구현 (#21)
    * feat: ProfileEditPage 컴포넌트 구현
    
    * feat: ProfileEdit 컴포넌트가 라우터 되도록 Root에 추가
    
    * feat: `useInput`hook 이 setInputValue 함수도 리턴하도록 수정
    
    * feat: ProfileEditPage 에서 각 input에 fetch로 데이터 초기화  및 onChange 이벤트 적용하는 기능 추가
    
    * refactor: profile_edit URL을 profile-edit 으로 수정
    
    * feat: ProfileContentItem 컴포넌트 구현
    
    * feat: ProfileEditPage 에서 ProfileContentItem 컴포넌트를 사용하도록 코드 수정
    
    * refactor: 코드 리뷰 피드백 반영
    
    - profileImage 단축 표기법으로 변경
    - 제출 버튼의 클래스명 변경
    - 프로필 사진도 동작하도록 pros로 넘겨주도록 추가

--- src/Root.jsx
+++ src/Root.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage';
 import DetailPage from '../src/pages/DetailPage/DetailPage';
+import ProfileEditPage from '../src/pages/ProfileEditPage/ProfileEditPage';
+
+
 
 const Root = () => {
   return (
@@ -11,6 +14,10 @@ const Root = () => {
         <Switch>
           <Route exact path="/" render={() => <MainPage />}></Route>
           <Route path="/post" render={() => <DetailPage />}></Route>
+          <Route
+            path="/profile-edit"
+            render={() => <ProfileEditPage />}
+          ></Route>
         </Switch>
       </Router>
     </>
---
+++ src/pages/ProfileEditPage/ProfileEditPage.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import './ProfileEditPage.scss';
+import Header from '../../components/Header/Header';
+import ProfileImage from '../../components/ProfileImage/ProfileImage';
+import ProfileContentItem from '../../components/ProfileContentItem/ProfileContentItem';
+import Button from 'react-bootstrap/Button';
+import useInput from '../../hooks/useInput';
+
+const ProfileEditPage = () => {
+  const [inputValue, setInputValue, handleChange, restore] = useInput();
+  const { profileImage, nickname, email, phone, introduction } = inputValue;
+
+  useEffect(() => {
+    async function fetchUsefInfo(url) {
+      const response = await fetch(url);
+      const userInfo = await response.json();
+      const { profileImage, nickname, email, phone } = userInfo;
+      const buf = Buffer.from(userInfo.description);
+      const initialValue = {
+        profileImage,
+        nickname,
+        email,
+        phone,
+        introduction: buf.toString()
+      };
+
+      setInputValue(initialValue);
+    }
+
+    fetchUsefInfo('http://13.124.93.76/user/info');
+  }, []);
+
+  return (
+    <>
+      <Header></Header>
+      <div className="profile-edit-page">
+        <div className="profile-edit-page-nav">
+          <div className="profile-edit-page-nav-item">프로필 편집</div>
+        </div>
+        <form className="profile-edit-page-content">
+          <div className="profile-edit-page-content-item">
+            <ProfileImage medium src={profileImage}></ProfileImage>
+            <button>프로필 사진 바꾸기</button>
+          </div>
+          <ProfileContentItem
+            label="닉네임"
+            value={nickname}
+            name="nickname"
+            changeHandler={handleChange}
+          ></ProfileContentItem>
+          <ProfileContentItem
+            label="소개"
+            value={introduction}
+            name="introduction"
+            changeHandler={handleChange}
+          ></ProfileContentItem>
+          <ProfileContentItem
+            label="이메일"
+            value={email}
+            name="email"
+            changeHandler={handleChange}
+          ></ProfileContentItem>
+          <ProfileContentItem
+            label="휴대폰 번호"
+            value={phone}
+            name="phone"
+            changeHandler={handleChange}
+          ></ProfileContentItem>
+          <Button variant="primary" size="lg" className="submit-btn">
+            제출
+          </Button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default ProfileEditPage;
