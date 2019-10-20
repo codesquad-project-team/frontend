12737ed661075364a89dcf766529f21d93d77818
Hwarang Lee <hwrng2@gmail.com>
2019-10-20 19:32:31 +0900

    [5] 회원가입 모달창 구현 / [7] 로그인 모달창 구현   (#20)
    * fix: RelatedPost에서 무한으로 fetch하는 버그 해결
    
    * delete: LoginPage 컴포넌트 삭제
    
    - login 기능은 페이지가 아니라 모달창으로 기획 수정하여 페이지 컴포넌트 삭제
    
    * feat: OAuthBtn 컴포넌트 구현
    
    - CommonModal 컴포넌트에서 불러와서 사용할 수 있도록 구현
    
    * feat: CommonModal 컴포넌트 구현
    
    * feat: Header에서 회원가입 버튼 클릭했을 때 모달창 뜨도록 기능 구현
    
    - 사용자가 로그인하지 않았고, 회원가입 버튼 클릭했을 때 CommonModal 컴포넌트가 렌더링
    
    * feat: Modal 창이 열렸을 때 배경 스크롤 막는 기능 추가
    
    - 그외 닫기 버튼 마크업 수정
    - 그외 이미 계정 있는지 확인하는 마크업 추가
    
    * feat: Header의 signin 버튼에 모달창 띄우는 이벤트 핸들링 및 렌더링 코드 추가
    
    * feat: CommonModal 컴포넌트가 signin, signup의 모달창에서 재사용되도록 구현
    
    * feat: OAuth 컴포넌트가 signin, signup의 모달창에서 재사용되도록 구현
    
    * etc: 콘솔 코드 삭제
    
    * refactor: herf -> href로 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
 import useInput from '../../hooks/useInput';
 import ProfileImage from '../ProfileImage/ProfileImage';
 import CommonBtn from './CommonBtn';
+import CommonModal from '../CommonModal/CommonModal';
 
-const isLoggedIn = true; //TODO: 로그인 기능 구현시 수정
+const isLoggedIn = false; //TODO: 로그인 기능 구현시 수정
 
 const Header = () => {
   const [inputValue, handleChange, restore] = useInput();
+  const [clickedSignup, setClickedSignup] = useState(false);
+  const [clickedSignin, setClickedSignin] = useState(false);
 
   const handleSubmit = e => {
     e.preventDefault();
     restore('searchBar');
+  };
+
+  const handleSignin = () => {
+    clickedSignin ? setClickedSignin(false) : setClickedSignin(true);
+  };
+
+  const handleSignup = () => {
+    clickedSignup ? setClickedSignup(false) : setClickedSignup(true);
   };
 
   return (
@@ -36,11 +47,29 @@ const Header = () => {
           <ProfileImage small />
         ) : (
           <>
-            <CommonBtn>로그인</CommonBtn>
-            <CommonBtn>회원가입</CommonBtn>
+            <CommonBtn className="signin-btn" onClick={handleSignin}>
+              로그인
+            </CommonBtn>
+            <CommonBtn className="signup-btn" onClick={handleSignup}>
+              회원가입
+            </CommonBtn>
           </>
         )}
       </div>
+
+      {!isLoggedIn && clickedSignin && (
+        <CommonModal
+          clickHandler={handleSignin}
+          target={'signin'}
+        ></CommonModal>
+      )}
+
+      {!isLoggedIn && clickedSignup && (
+        <CommonModal
+          clickHandler={handleSignup}
+          target={'signup'}
+        ></CommonModal>
+      )}
     </div>
   );
 };
