b3f8277fd9b6045dc19c84e177f1ee18d4bbccdf
AllenK <revenoir7@gmail.com>
2019-10-24 16:40:42 +0900

    etc: api서버 url 업데이트

--- src/configs.js
+++ src/configs.js
@@ -1,7 +1,7 @@
 export const DEFAULT_PROFILE_IMG_URL =
   'https://user-images.githubusercontent.com/42905468/66740814-132e4e00-eeaf-11e9-8132-95c3a5594af0.png';
 
-export const WEB_SERVER_URL = 'http://13.124.93.76';
+export const WEB_SERVER_URL = 'http://13.124.93.76/apiv1';
 
 export const VIEWPORT_HEIGHT = Math.max(
   document.documentElement.clientHeight,
