84d23f78995f818d7799bd87e0f794e2db11be65
leehwarang <hwrng2@gmail.com>
2019-10-25 11:57:28 +0900

    refactor/style: fetch 코드 리팩토링, 로딩시 스피너 보이도록 스타일 추가

--- src/pages/ProfileEditPage/ProfileEditPage.jsx
+++ src/pages/ProfileEditPage/ProfileEditPage.jsx
@@ -5,30 +5,31 @@ import ProfileImage from '../../components/ProfileImage/ProfileImage';
 import ProfileContentItem from '../../components/ProfileContentItem/ProfileContentItem';
 import Button from 'react-bootstrap/Button';
 import useInput from '../../hooks/useInput';
+import useFetch from '../../hooks/useFetch';
+import { WEB_SERVER_URL, MAIN_COLOR } from '../../configs';
+import { css } from '@emotion/core';
+import FadeLoader from 'react-spinners/FadeLoader';
 
 const ProfileEditPage = () => {
   const [inputValue, setInputValue, handleChange, restore] = useInput();
   const { profileImage, nickname, email, phone, introduction } = inputValue;
 
-  useEffect(() => {
-    async function fetchUsefInfo(url) {
-      const response = await fetch(url);
-      const userInfo = await response.json();
-      const { profileImage, nickname, email, phone } = userInfo;
-      const buf = Buffer.from(userInfo.description);
-      const initialValue = {
-        profileImage,
-        nickname,
-        email,
-        phone,
-        introduction: buf.toString()
-      };
+  const { loading, error } = useFetch(`${WEB_SERVER_URL}/user/info`, {}, json =>
+    initUserInfo(json)
+  );
 
-      setInputValue(initialValue);
-    }
-
-    fetchUsefInfo('http://13.124.93.76/user/info');
-  }, []);
+  const initUserInfo = userInfo => {
+    const { profileImage, nickname, email, phone } = userInfo;
+    const buf = Buffer.from(userInfo.description);
+    const initialValue = {
+      profileImage,
+      nickname,
+      email,
+      phone,
+      introduction: buf.toString()
+    };
+    setInputValue(initialValue);
+  };
 
   return (
     <>
@@ -38,39 +39,50 @@ const ProfileEditPage = () => {
           <div className="profile-edit-page-nav">
             <div className="profile-edit-page-nav-item">프로필 편집</div>
           </div>
-          <form className="profile-edit-page-content-form">
-            <div className="profile-edit-page-content-item">
-              <ProfileImage medium src={profileImage}></ProfileImage>
-              <button>프로필 사진 바꾸기</button>
-            </div>
-            <ProfileContentItem
-              label="닉네임"
-              value={nickname}
-              name="nickname"
-              changeHandler={handleChange}
-            ></ProfileContentItem>
-            <ProfileContentItem
-              label="소개"
-              value={introduction}
-              name="introduction"
-              changeHandler={handleChange}
-            ></ProfileContentItem>
-            <ProfileContentItem
-              label="이메일"
-              value={email}
-              name="email"
-              changeHandler={handleChange}
-            ></ProfileContentItem>
-            <ProfileContentItem
-              label="휴대폰 번호"
-              value={phone}
-              name="phone"
-              changeHandler={handleChange}
-            ></ProfileContentItem>
-            <Button type="submit" className="submit-btn">
-              제출
-            </Button>
-          </form>
+          <FadeLoader
+            css={override}
+            sizeUnit={'px'}
+            size={150}
+            color={MAIN_COLOR}
+            loading={loading}
+          />
+          {!loading && (
+            <>
+              <form className="profile-edit-page-content-form">
+                <div className="profile-edit-page-content-item">
+                  <ProfileImage medium src={profileImage}></ProfileImage>
+                  <button>프로필 사진 바꾸기</button>
+                </div>
+                <ProfileContentItem
+                  label="닉네임"
+                  value={nickname}
+                  name="nickname"
+                  changeHandler={handleChange}
+                ></ProfileContentItem>
+                <ProfileContentItem
+                  label="소개"
+                  value={introduction}
+                  name="introduction"
+                  changeHandler={handleChange}
+                ></ProfileContentItem>
+                <ProfileContentItem
+                  label="이메일"
+                  value={email}
+                  name="email"
+                  changeHandler={handleChange}
+                ></ProfileContentItem>
+                <ProfileContentItem
+                  label="휴대폰 번호"
+                  value={phone}
+                  name="phone"
+                  changeHandler={handleChange}
+                ></ProfileContentItem>
+                <Button type="submit" className="submit-btn">
+                  제출
+                </Button>
+              </form>
+            </>
+          )}
         </div>
       </div>
     </>
@@ -78,3 +90,8 @@ const ProfileEditPage = () => {
 };
 
 export default ProfileEditPage;
+
+const override = css`
+  display: block;
+  margin: 17rem auto;
+`;
