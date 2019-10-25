3d6108f1f8d987c29c7872a02de5ad7381c80d96
leehwarang <hwrng2@gmail.com>
2019-10-25 20:14:19 +0900

    refactor: 불필요한 <></> 코드 삭제

--- src/pages/ProfileEditPage/ProfileEditPage.jsx
+++ src/pages/ProfileEditPage/ProfileEditPage.jsx
@@ -47,41 +47,39 @@ const ProfileEditPage = () => {
             loading={loading}
           />
           {!loading && (
-            <>
-              <form className="profile-edit-page-content-form">
-                <div className="profile-edit-page-content-item">
-                  <ProfileImage medium src={profileImage}></ProfileImage>
-                  <button>프로필 사진 바꾸기</button>
-                </div>
-                <ProfileContentItem
-                  label="닉네임"
-                  value={nickname}
-                  name="nickname"
-                  changeHandler={handleChange}
-                ></ProfileContentItem>
-                <ProfileContentItem
-                  label="소개"
-                  value={introduction}
-                  name="introduction"
-                  changeHandler={handleChange}
-                ></ProfileContentItem>
-                <ProfileContentItem
-                  label="이메일"
-                  value={email}
-                  name="email"
-                  changeHandler={handleChange}
-                ></ProfileContentItem>
-                <ProfileContentItem
-                  label="휴대폰 번호"
-                  value={phone}
-                  name="phone"
-                  changeHandler={handleChange}
-                ></ProfileContentItem>
-                <Button type="submit" className="submit-btn">
-                  제출
-                </Button>
-              </form>
-            </>
+            <form className="profile-edit-page-content-form">
+              <div className="profile-edit-page-content-item">
+                <ProfileImage medium src={profileImage}></ProfileImage>
+                <button>프로필 사진 바꾸기</button>
+              </div>
+              <ProfileContentItem
+                label="닉네임"
+                value={nickname}
+                name="nickname"
+                changeHandler={handleChange}
+              ></ProfileContentItem>
+              <ProfileContentItem
+                label="소개"
+                value={introduction}
+                name="introduction"
+                changeHandler={handleChange}
+              ></ProfileContentItem>
+              <ProfileContentItem
+                label="이메일"
+                value={email}
+                name="email"
+                changeHandler={handleChange}
+              ></ProfileContentItem>
+              <ProfileContentItem
+                label="휴대폰 번호"
+                value={phone}
+                name="phone"
+                changeHandler={handleChange}
+              ></ProfileContentItem>
+              <Button type="submit" className="submit-btn">
+                제출
+              </Button>
+            </form>
           )}
         </div>
       </div>
