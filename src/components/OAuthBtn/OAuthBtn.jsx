98ee60228bc47ce734e9b2a2bdc6863db9a8f937
leehwarang <hwrng2@gmail.com>
2019-10-28 18:01:10 +0900

    feat: 카카오 OAuth btn에 연결 링크 추가

--- src/components/OAuthBtn/OAuthBtn.jsx
+++ src/components/OAuthBtn/OAuthBtn.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './OAuthBtn.scss';
+import { WEB_SERVER_URL } from '../../configs';
 
 const OAuthBtn = ({ company, imgUrl, msg }) => {
   return (
     <div className="OAuth-btn">
       <img src={imgUrl} />
-      <a className="OAuth-btn-text" href="">
+      <a className="OAuth-btn-text" href={`${WEB_SERVER_URL}/auth/kakao`}>
         {company} 계정으로 {msg}
       </a>
     </div>
