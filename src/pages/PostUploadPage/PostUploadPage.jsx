bcf40a574ce8ac7743715716f06922f6762f7f93
AllenK <revenoir7@gmail.com>
2019-10-31 19:34:55 +0900

    feat: 필요한 엘리먼트들 적당히 배치

--- src/pages/PostUploadPage/PostUploadPage.jsx
+++ src/pages/PostUploadPage/PostUploadPage.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PostUploadPage.scss';
 import CommonCard from '../../components/CommonCard/CommonCard';
 import Header from '../../components/Header/Header';
+import CommonBtn from '../../components/CommonBtn/CommonBtn';
 
 const PostUploadPage = () => {
+  const [select, setSelect] = useState('');
+  const handleChange = ({ target }) => {
+    setSelect(target.value);
+  };
   return (
     <>
       <Header />
       <CommonCard.background>
-        <CommonCard large>
-          <input type="file" />
-          <input type="text" placeholder="어디에서" />
-          <input type="text" placeholder="누구랑" />
-          <input type="text" placeholder="뭐하기" />
-          <textarea name="comment" id="" cols="30" rows="10" />
+        <CommonCard large className="post-upload-page">
+          <div className="post-upload-page-image">
+            <input type="file" className="post-upload-page-image-input" />
+          </div>
+          <button>장소검색</button>
+          <div className="post-upload-page-title">
+            <input type="text" placeholder="어디" />
+            <span>에서</span>
+            <select name="companion" value={select} onChange={handleChange}>
+              <option value="" disabled>
+                누구랑
+              </option>
+              <option value="혼자서">혼자서</option>
+              <option value="친구랑">친구랑</option>
+              <option value="연인이랑">연인이랑</option>
+            </select>
+            <input type="text" placeholder="뭐하기" />
+          </div>
+          <textarea
+            name="comment"
+            className="post-upload-page-comment"
+            placeholder="간단한 코멘트를 남겨주세요."
+          />
+          <div className="post-upload-page-btns">
+            <CommonBtn>작성</CommonBtn>
+            <CommonBtn>취소</CommonBtn>
+          </div>
         </CommonCard>
       </CommonCard.background>
     </>
