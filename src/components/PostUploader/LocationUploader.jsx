d87306b6d15a162f6cc6fae355447e8cbf036213
AllenK <revenoir7@gmail.com>
2019-11-02 09:26:56 +0900

    feat: useModal hooks 작성
    - modal 로직을 재사용할 수 있도록 커스텀훅 작성

--- src/components/PostUploader/LocationUploader.jsx
+++ src/components/PostUploader/LocationUploader.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useModal from './Modal';
 
 const LocationUploader = () => {
+  const { Modal, handleClick, open } = useModal();
   return (
     <div>
-      <button>장소검색</button>
+      <button onClick={handleClick}>장소검색</button>
+      {open && (
+        <Modal onClick={handleClick}>
+          <div style={{ width: '100px', height: '100px', background: '#fff' }}>
+            test
+            <button onClick={handleClick}>close</button>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
