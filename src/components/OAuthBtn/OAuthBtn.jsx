cb8cca422d03f253be6965bc2090dad38dfa7aa3
AllenK <revenoir7@gmail.com>
2019-11-07 17:06:55 +0900

    feat: 카카오 로그인/회원가입 버튼에 href 추가

--- src/components/OAuthBtn/OAuthBtn.jsx
+++ src/components/OAuthBtn/OAuthBtn.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './OAuthBtn.scss';
 import CommonBtn from '../CommonBtn/CommonBtn';
 
-const OAuthBtn = ({ company, imgUrl, msg }) => {
+const OAuthBtn = ({ company, imgUrl, msg, href }) => {
   return (
-    <CommonBtn className="OAuth-btn" styleType="none">
-      <img src={imgUrl} />
-      {company} 계정으로 {msg}
-    </CommonBtn>
+    <a href={href} className="OAuth-btn-link">
+      <CommonBtn className="OAuth-btn" styleType="none">
+        <img src={imgUrl} />
+        {company} 계정으로 {msg}
+      </CommonBtn>
+    </a>
   );
 };
 
