e473bc868315a7cc3d9415efdf5dc57c34bc9e16
AllenK <revenoir7@gmail.com>
2019-11-11 01:18:59 +0900

    feat: 사용자의 profile-content를 fetch를 통해 받아오는 기능 추가

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -7,29 +7,23 @@ import NewPostBtn from '../../components/NewPostBtn/NewPostBtn';
 import useFetch from '../../hooks/useFetch';
 import { WEB_SERVER_URL } from '../../configs';
 
-const data = {
-  isMyProfile: true,
-  nickname: 'Mi-Shell',
-  totalPost: 5331,
-  totalFollower: 10128,
-  totalFollowing: 1079,
-  introduction: 'Coffee Holic. Love Beer.',
-  profileImage:
-    'https://team-project-s3-bucket.s3.ap-northeast-2.amazonaws.com/profile-images/dummyUser%40google.com/myProfile.png'
-};
-
 const ProfilePage = props => {
-  //TODO: api완성 시 수정 예정
+  //TODO: userId를 넘겨받아서 요청보내도록 수정 예정
   // const userId = props.userId //match.params에서 넘겨주는 userId
-  // const [data, setData] = useState(null);
-  // const {error, loading} = useFetch(`${WEB_SERVER_URL}/someapi...`, {}, setData)
+  const userId = 1;
+  const [data, setData] = useState(null);
+  const { error, loading } = useFetch(
+    `${WEB_SERVER_URL}/user/profile-content?id=${userId}`,
+    {},
+    setData
+  );
   return (
     <div className="profile-page">
       <Header />
       <Link to="/post/upload">
         <NewPostBtn />
       </Link>
-      <ProfileInfo data={data} />
+      {data && <ProfileInfo data={data} />}
       <PostContainer api="/post?page=" />
     </div>
   );
