f61e72168adbe9e07e7cf9ed4ec86c5e07735e29
AllenK <revenoir7@gmail.com>
2019-11-12 23:02:26 +0900

    feat: SignupPage 컴포넌트 파일 생성 및 라우터 경로 추가

---
+++ src/pages/SignupPage/SignupPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignupPage = () => {
+  return (
+    <div>
+      <input type="text" placeholder="4~15자로 입력해주세요" />
+    </div>
+  );
+};
+
+export default SignupPage;
