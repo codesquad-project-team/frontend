8152192aa839c0bf168ccee23544d5298d0f2b3b
AllenK <revenoir7@gmail.com>
2019-11-14 17:36:27 +0900

    style: 포스트 상세 페이지의 InfoWindow 레이아웃 조정
    - 카카오 지도로 바꾸면서 변경된 레이아웃 디테일 수정

--- src/components/MapView/InfoWindow.jsx
+++ src/components/MapView/InfoWindow.jsx
@@ -1,59 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './InfoWindow.scss';
+import CloseBtn from '../CommonBtn/CloseBtn';
 import { IMAGE_BUCKET_URL } from '../../configs';
 
 const InfoWindow = props => {
   const {
-    locationName,
+    titlePlace,
     locationAddress,
     locationPhoneNumber,
     locationLinkAddress
   } = props.info;
 
   return (
-    <>
-      <div className="info-window">
-        <h3 className="info-window-location-name">
-          <a
-            href={locationLinkAddress}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {locationName}
-          </a>
-        </h3>
-        <hr />
-        <div>{locationAddress}</div>
-        {/* <img
+    <div className="info-window">
+      <h3 className="info-window-location-name">
+        <a href={locationLinkAddress} target="_blank" rel="noopener noreferrer">
+          {titlePlace}
+        </a>
+      </h3>
+      <hr />
+      <div>{locationAddress}</div>
+      {/* <img
           src=""
           width="55"
           height="55"
-          alt={locationName}
+          alt={titlePlace}
           className="info-window-thumbnail"
         />
         <br />  */}
-        <div>{locationPhoneNumber}</div>
-        <span>
-          <a
-            href={locationLinkAddress}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {locationLinkAddress}
-          </a>
-        </span>
-      </div>
+      <div>{locationPhoneNumber}</div>
+      <span>
+        <a href={locationLinkAddress} target="_blank" rel="noopener noreferrer">
+          {locationLinkAddress}
+        </a>
+      </span>
       <img
         className="info-window-arrow-image"
         src={`${IMAGE_BUCKET_URL}/info-window-arrow.png`}
         alt=""
       />
-      <img
-        className="info-window-close-image"
-        src={`${IMAGE_BUCKET_URL}/times-solid.png`}
-      />
-    </>
+      <CloseBtn />
+    </div>
   );
 };
 
@@ -61,7 +49,7 @@ export default InfoWindow;
 
 InfoWindow.propTypes = {
   info: PropTypes.shape({
-    locationName: PropTypes.string,
+    titlePlace: PropTypes.string,
     locationAddress: PropTypes.string,
     locationPhoneNumber: PropTypes.string,
     locationLinkAddress: PropTypes.string
