3e4fe147b2b895920eba875f86907718c40ad923
AllenK <revenoir7@gmail.com>
2019-11-14 09:58:25 +0900

    feat: 임시토큰 검증기능 추가

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -3,25 +3,24 @@ import './SignupPage.scss';
 import CommonBtn from '../../components/CommonBtn/CommonBtn';
 import useInput from '../../hooks/useInput';
 import useFetch from '../../hooks/useFetch';
+import { css } from '@emotion/core';
+import FadeLoader from 'react-spinners/FadeLoader';
 import { debounce } from '../../utils/utils';
-import { WEB_SERVER_URL } from '../../configs';
-
-const authData = {
-  provider: '카카오'
-};
+import { WEB_SERVER_URL, MAIN_COLOR } from '../../configs';
 
 const SignupPage = () => {
   const { inputValue, handleChange } = useInput();
   const { nickname } = inputValue;
 
   //임시 토큰 검증
-  // const [authData, setAuthData] = useState(null);
-  // const { loading } = useFetch(
-  //   `${WEB_SERVER_URL}/validate/tempToken`,
-  //   { method: 'POST', credentials: 'include' },
-  //   json => setAuthData(json)
-  // );
-  const { provider } = authData || { provider: '' };
+  const [authData, setAuthData] = useState(null);
+  const { loading } = useFetch(
+    `${WEB_SERVER_URL}/validate/tempToken`,
+    { method: 'POST', credentials: 'include' },
+    json => setAuthData(json)
+  );
+
+  const { provider } = authData || { provider: '(테스트)' }; //TODO: 토큰검증 기능 안정되면 제거
   const postposition = provider === '카카오' ? '로' : '으로';
 
   const [reason, setReason] = useState(null);
@@ -89,39 +88,55 @@ const SignupPage = () => {
       <header>
         <h1>Connect Flavor</h1>
       </header>
-      <div className="signup-page-body">
-        <h2>
-          {provider}
-          {postposition} 회원가입
-        </h2>
-        <div className="signup-page-auth-checker">인증완료</div>
-        <span>닉네임을 만들어주세요</span>
-        <div className="signup-page-input-section">
-          <input
-            name="nickname"
-            value={nickname}
-            onChange={handleChange}
-            type="text"
-            placeholder="4~15자로 입력해주세요."
-          />
-          <div>
-            {reason && (
-              <span
-                className={
-                  reason.valid ? 'signup-page-reason-ok' : 'signup-page-reason'
-                }
-              >
-                {reason.message}
-              </span>
-            )}
+      {!loading && (
+        <div className="signup-page-body">
+          <h2>
+            {provider}
+            {postposition} 회원가입
+          </h2>
+          <div className="signup-page-auth-checker">인증완료</div>
+          <span>닉네임을 만들어주세요</span>
+          <div className="signup-page-input-section">
+            <input
+              name="nickname"
+              value={nickname}
+              onChange={handleChange}
+              type="text"
+              placeholder="4~15자로 입력해주세요."
+            />
+            <div>
+              {reason && (
+                <span
+                  className={
+                    reason.valid
+                      ? 'signup-page-reason-ok'
+                      : 'signup-page-reason'
+                  }
+                >
+                  {reason.message}
+                </span>
+              )}
+            </div>
           </div>
+          <CommonBtn styleType="normal" className="signup-page-signup-btn">
+            회원가입
+          </CommonBtn>
         </div>
-        <CommonBtn styleType="normal" className="signup-page-signup-btn">
-          회원가입
-        </CommonBtn>
-      </div>
+      )}
+      <FadeLoader
+        css={override}
+        sizeUnit={'px'}
+        size={150}
+        color={MAIN_COLOR}
+        loading={loading}
+      />
     </div>
   );
 };
 
 export default SignupPage;
+
+const override = css`
+  display: block;
+  margin: 17rem auto;
+`;
