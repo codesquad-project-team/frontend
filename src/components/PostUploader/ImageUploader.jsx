5c615ad98f2fd2ba6be312b6120c2aa469c8d1ff
leehwarang <hwrng2@gmail.com>
2019-11-15 21:56:05 +0900

    feat: 파일추가시, 사용자가 선택한 이미지 파일을 더하고 화면에서 미리보는 기능 추가

--- src/components/PostUploader/ImageUploader.jsx
+++ src/components/PostUploader/ImageUploader.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import './ImageUploader.scss';
+import React, { useState } from "react";
+import "./ImageUploader.scss";
+import { PRE_SIGNED_URL } from "../../configs";
 
-const ImageUploader = () => {
+const ImageUploader = ({
+  selectedImages,
+  addImageHandler,
+  deleteImageHandler
+}) => {
+  const getImage = ({ target }) => {
+    const files = Array.from(target.files);
+    addImageHandler(files);
+  };
+
+  const previewImages = selectedImages.map((image, index) => {
+    const previewUrl = URL.createObjectURL(image);
+    return (
+      <li key={index}>
+        <img className="image-uploader-preview-img" src={previewUrl} />
+      </li>
+    );
+  });
+
+  const uploadFile = async e => {
+    e.preventDefault();
+  };
+
   return (
     <div className="image-uploader">
-      <input type="file" className="image-uploader-input" />
+      <ul className="image-uploader-preview" />
+      {previewImages}
+      <input
+        id="upload-image"
+        type="file"
+        accept="image/*"
+        className="image-uploader-input"
+        onChange={getImage}
+        style={{ display: !previewImages.length ? "block" : "none" }}
+        multiple
+        name="filename[]"
+      />
     </div>
   );
 };
--- src/pages/PostUploadPage/PostUploadPage.jsx
+++ src/pages/PostUploadPage/PostUploadPage.jsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import './PostUploadPage.scss';
-import CommonPost from '../../components/CommonPost/CommonPost';
-import Header from '../../components/Header/Header';
-import ImageUploader from '../../components/PostUploader/ImageUploader';
-import LocationUploader from '../../components/PostUploader/LocationUploader';
-import TitleUploader from '../../components/PostUploader/TitleUploader';
-import CommentUploader from '../../components/PostUploader/CommentUploader';
-import PostQuestions from '../../components/PostUploader/PostQuestions';
-import CommonBtn from '../../components/CommonBtn/CommonBtn';
+import React, { useState } from "react";
+import "./PostUploadPage.scss";
+import CommonPost from "../../components/CommonPost/CommonPost";
+import Header from "../../components/Header/Header";
+import ImageUploader from "../../components/PostUploader/ImageUploader";
+import LocationUploader from "../../components/PostUploader/LocationUploader";
+import TitleUploader from "../../components/PostUploader/TitleUploader";
+import CommentUploader from "../../components/PostUploader/CommentUploader";
+import PostQuestions from "../../components/PostUploader/PostQuestions";
+import CommonBtn from "../../components/CommonBtn/CommonBtn";
 
 const PostUploadPage = () => {
+  const [selectedImages, setSelectedImages] = useState([]);
+  const addImageHandler = newImages => {
+    setSelectedImages([...newImages, ...selectedImages]);
+  };
+
+  const deleteImageHandler = () => {
+    // images에서 입력한 image 삭제하고 setImages
+  };
+
   const handleSubmit = e => {
     e.preventDefault();
   };
+
+  console.log(selectedImages);
+
   return (
     <>
       <Header />
       <CommonPost.background className="post-upload-page-background">
         <CommonPost large className="post-upload-page">
-          <ImageUploader />
+          <ImageUploader
+            selectedImages={selectedImages}
+            addImageHandler={addImageHandler}
+            deleteImageHandler={deleteImageHandler}
+          />
           <LocationUploader />
           <TitleUploader />
           <CommentUploader />
