a710b3e2fff0033fdc65bb136220ecd12ef5cdc6
Heungsoo, Kim <revenoir7@gmail.com>
2019-11-18 23:23:56 +0900

    [25] 회원가입 페이지(닉네임입력) 구현 (#59)
    * feat: SignupPage 컴포넌트 파일 생성 및 라우터 경로 추가
    
    * feat/style: 필요한 엘리먼트 배치 및 스타일링
    
    * feat: input태그에 useInput 적용
    
    * feat: 입력한 닉네임을 서버로 보내는 기능 작성(디바운스처리)
    
    - debounce 유틸함수 작성
    - input에 입력이 있을 때 디바운스 처리된 fetch함수를 실행하도록 작성
    - debounce의 클로저 스코프를 기존과 동일하게 유지하기 위해 useCallback 사용
    
    * feat: 입력값 유효성 검사 및 res.status에 따라 메시지 표시기능추가
    
    * refactor: 가독성을 위한 코드 순서 및 함수명 수정
    
    * style/refactor: reason메시지에 폰트컬러 적용
    
    - reason state를 객체형태로 두어서 사용가능여부를 확인할 수 있도록 작성
    - 이미 보내진 fetch요청을 취소할 수 없는 버그발견
    
    * feat: 임시토큰 검증기능 추가
    
    * refactor: checkNickname 함수명을 좀 더 역할에 가깝게 수정
    
    * refactor: 불필요한 주석 제거
    
    * refactor: 가독성 개선을 위한 변수명 수정 및 문법 수정
    
    - 닉네임유효성 여부를 나타내는 변수명 수정
     : reason > nicknameValidity
    - if-else문을 switch문으로 수정
    - 버튼 styleType을 emphasize로 강조

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -17,3 +17,11 @@ export const throttle = (callback, delay = 200) => {
     }
   };
 };
+
+export const debounce = (callback, delay = 300) => {
+  let timer;
+  return (...param) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => callback(...param), delay);
+  };
+};
