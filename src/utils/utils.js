0ff74088047e6e58cb34900a996e2f83b522cf46
leehwarang <hwrng2@gmail.com>
2019-11-19 16:46:01 +0900

    feat: date 객체를 YYYYMMMDDHHMMSS 형태의 string으로 반환하는 함수 추가

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 export const getClassName = ({ props, prefix }) => {
   const { small, medium, large } = props;
-  let className = '';
+  let className = "";
   if (small) className = `${prefix}-small`;
   if (medium) className = `${prefix}-medium`;
   if (large) className = `${prefix}-large`;
@@ -16,4 +16,18 @@ export const throttle = (callback, delay = 200) => {
       return callback(...param);
     }
   };
+};
+
+export const YYYYMMDDHHMMSS = dateObj => {
+  function pad2(n) {
+    return (n < 10 ? "0" : "") + n;
+  }
+  return (
+    dateObj.getFullYear() +
+    pad2(dateObj.getMonth() + 1) +
+    pad2(dateObj.getDate()) +
+    pad2(dateObj.getHours()) +
+    pad2(dateObj.getMinutes()) +
+    pad2(dateObj.getSeconds())
+  );
 };
