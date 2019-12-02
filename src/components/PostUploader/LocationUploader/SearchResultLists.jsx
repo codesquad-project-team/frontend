57c645400b3f3de7e859ebf992014738e553141f
AllenK <revenoir7@gmail.com>
2019-12-03 00:39:00 +0900

    chore: 불필요한 주석 제거

--- src/components/PostUploader/LocationUploader/SearchResultLists.jsx
+++ src/components/PostUploader/LocationUploader/SearchResultLists.jsx
@@ -36,23 +36,7 @@ const SearchResultLists = ({
     }
   };
   const results = makeResults(searchResult);
-  // const results = isEmptyArray(searchResult) ? (
-  //   <div className="search-result-lists-no-result">검색결과가 없습니다.</div>
-  // ) : (
-  //   searchResult.map((item, index) => (
-  //     <div
-  //       key={item.x}
-  //       data-index={index}
-  //       className={`search-result-lists-item ${
-  //         selectedIndex === index ? 'search-result-lists-selected-item' : ''
-  //       }`}
-  //       onClick={handleClick}
-  //     >
-  //       <div className="search-result-lists-name">{item.place_name}</div>
-  //       <div className="search-result-lists-address">{item.address_name}</div>
-  //     </div>
-  //   ))
-  // );
+
   return <div className={className}>{results}</div>;
 };
 
