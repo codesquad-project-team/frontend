3b2b8c891ed16d0fa436587f0f8f7887c62a1f8f
AllenK <revenoir7@gmail.com>
2019-12-03 18:32:44 +0900

    refactor: 핸들러함수 이름을 역할에 맞게 수정

--- src/components/PostUploader/LocationUploader/LocationFindButton.jsx
+++ src/components/PostUploader/LocationUploader/LocationFindButton.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './LocationFindButton.scss';
 import { IMAGE_BUCKET_URL } from '../../../configs';
 
-const LocationFindButton = ({ onClick, children }) => {
+const LocationFindButton = ({ onClick: openLocationFinder, children }) => {
   return (
-    <button className="location-find-button" onClick={onClick}>
+    <button className="location-find-button" onClick={openLocationFinder}>
       <img
         className="location-find-button-icon"
         src={`${IMAGE_BUCKET_URL}/find-location-icon8.png`}
