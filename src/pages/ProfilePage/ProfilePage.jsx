9e80d8ff968807dd7b28f876980fd216dbd2ccbd
AllenK <revenoir7@gmail.com>
2019-12-04 21:25:36 +0900

    feat: 내 프로필 여부를 LoginContext의 id를 이용해 판단하는 기능 추가
    - 기존에는 userId와 토큰을 보내서 서버에서 판단하도록 했지만 토큰의 정보를 클라이언트에 가지고 있기 때문에 불필요하게 됨

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -8,15 +8,21 @@ import useFetch from '../../hooks/useFetch';
 import { css } from '@emotion/core';
 import FadeLoader from 'react-spinners/FadeLoader';
 import { WEB_SERVER_URL, MAIN_COLOR } from '../../configs';
+import { useLoginContext } from '../../contexts/LoginContext';
 
-const ProfilePage = props => {
-  //TODO: userId를 넘겨받아서 요청보내도록 수정 예정
-  // const userId = props.userId //match.params에서 넘겨주는 userId
-  const userId = 1;
+const ProfilePage = () => {
+  //TODO: userId를 전역 context에서 받아서 profile-content api 요청하기
+  //라우터URL을 nickname으로 표시하기 때문에 prop으로는 userId를 받을 수 없음.
+  //따라서 userId는 별도의 방법으로 받아야함. 전역 context를 사용해야할 듯.
+  //const { userId } = useContext(someContext);
+  const userId = 103;
+  const { id } = useLoginContext();
+  const isMyProfile = id === userId;
+
   const [data, setData] = useState(null);
   const { error, loading } = useFetch(
     `${WEB_SERVER_URL}/user/profile-content?id=${userId}`,
-    {},
+    { credentials: 'include' },
     setData
   );
   return (
@@ -32,7 +38,7 @@ const ProfilePage = props => {
         color={MAIN_COLOR}
         loading={loading}
       />
-      {data && <ProfileInfo data={data} />}
+      {!loading && <ProfileInfo data={data} isMyProfile={isMyProfile} />}
       <PostContainer api="/post?page=" />
     </div>
   );
