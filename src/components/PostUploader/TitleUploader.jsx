38c578711d48707791cd745fbcbee2124b7695ab
AllenK <revenoir7@gmail.com>
2019-12-12 23:20:20 +0900

    feat: TitleUploader의 companion부분 인터렉션 구현
    - 혼자서, 친구랑, 연인이랑, 가족이랑, 직접 입력 5개의 옵션 부여
    - 직접 입력 선택 시 select box를 input box로 전환
    - 직접 입력 상태에서 다시 선택으로 돌아가고 싶은 경우를 위해 input focus 시 select box를 띄워줌
    - 렌더링 변화가 필요한 시점을 state로 관리.
      이 때, react dev tool에서 state key를 보기 위해 object형태로 작성

--- src/components/PostUploader/TitleUploader.jsx
+++ src/components/PostUploader/TitleUploader.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import './TitleUploader.scss';
 import useInput from '../../hooks/useInput';
 
@@ -8,33 +8,105 @@ const TitleUploader = ({ placeName }) => {
     setInputValue,
     handleChange: handleInputChange
   } = useInput();
-  const { titlePlace } = inputValue;
+  const { location, companion, activity } = inputValue;
 
-  useMemo(() => setInputValue({ titlePlace: placeName }), [placeName]);
+  useMemo(() => setInputValue({ location: placeName }), [placeName]);
 
   const [select, setSelect] = useState('');
-  const handleChange = ({ target }) => {
+  const [state, setState] = useState({});
+  const { showsFreeInput, isCompanionInputFocused, isOverlayHovered } = state;
+
+  const handleSelectBoxChange = ({ target }) => {
     setSelect(target.value);
   };
+
+  const setInputStateFocused = () => {
+    setState({ ...state, isCompanionInputFocused: true });
+  };
+
+  const setInputStateBlurred = () => {
+    setState({ ...state, isCompanionInputFocused: false });
+  };
+
+  const setOverlayStateHovered = () => {
+    setState({ ...state, isOverlayHovered: true });
+  };
+
+  const setOverlayStateUnhovered = () => {
+    setState({ ...state, isOverlayHovered: false });
+  };
+
+  useEffect(() => {
+    if (select === 'free_input') {
+      setState({ ...state, showsFreeInput: true });
+    } else {
+      setState({});
+    }
+  }, [select]);
+
   return (
     <div className="title-uploader">
       <input
         type="text"
         placeholder="어디"
-        name="titlePlace"
-        value={titlePlace}
+        name="location"
+        value={location}
         onChange={handleInputChange}
       />
       <span>에서</span>
-      <select name="companion" value={select} onChange={handleChange}>
-        <option value="" disabled>
-          누구랑
-        </option>
-        <option value="혼자서">혼자서</option>
-        <option value="친구랑">친구랑</option>
-        <option value="연인이랑">연인이랑</option>
-      </select>
-      <input type="text" placeholder="뭐하기" />
+      <span className="title-uploader-companion-section">
+        {showsFreeInput ? (
+          <input
+            type="text"
+            placeholder="누구랑"
+            name="companion"
+            value={companion}
+            onChange={handleInputChange}
+            onFocus={setInputStateFocused}
+            onBlur={setInputStateBlurred}
+          />
+        ) : (
+          <select
+            name="companion"
+            value={select}
+            onChange={handleSelectBoxChange}
+          >
+            <option value="" disabled>
+              누구랑
+            </option>
+            <option value="혼자서">혼자서</option>
+            <option value="친구랑">친구랑</option>
+            <option value="연인이랑">연인이랑</option>
+            <option value="가족이랑">가족이랑</option>
+            <option value="free_input">직접입력</option>
+          </select>
+        )}
+        {isOverlayHovered || isCompanionInputFocused ? (
+          <select
+            className="title-uploader-overlay-select-box"
+            name="companion"
+            value={select}
+            onChange={handleSelectBoxChange}
+            onMouseOver={setOverlayStateHovered}
+            onMouseLeave={setOverlayStateUnhovered}
+          >
+            <option value="free_input" disabled>
+              누구랑
+            </option>
+            <option value="혼자서">혼자서</option>
+            <option value="친구랑">친구랑</option>
+            <option value="연인이랑">연인이랑</option>
+            <option value="가족이랑">가족이랑</option>
+          </select>
+        ) : null}
+      </span>
+      <input
+        type="text"
+        placeholder="뭐하기"
+        name="activity"
+        value={activity}
+        onChange={handleInputChange}
+      />
     </div>
   );
 };
