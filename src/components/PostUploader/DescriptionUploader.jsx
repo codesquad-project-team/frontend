d419467bd60f1bb04a0534609f79025ecdeac6fa
AllenK <revenoir7@gmail.com>
2019-12-14 23:25:28 +0900

    feat: 글자수(byte) count해서 limit초과 시 화면에 표시하는 로직 구현
    - style작업은 별도 커밋으로 진행

--- src/components/PostUploader/DescriptionUploader.jsx
+++ src/components/PostUploader/DescriptionUploader.jsx
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DescriptionUploader.scss';
 
-const DescriptionUploader = ({ description, setDescription }) => {
-  const countBytes = texts => {};
+const MAX_DESCRIPTION_BYTES = 100;
+
+const DescriptionUploader = ({
+  description,
+  setDescription,
+  setReadyToUpload
+}) => {
+  const [showsOverLimitMessage, setShowsOverLimitMessage] = useState(false);
+
+  const countBytes = texts => {
+    const len = texts.length;
+    let totalBytes = 0;
+
+    for (let index = 0; index < len; index++) {
+      const isHangeul = texts.charCodeAt(index) > 128 ? true : false;
+      isHangeul ? (totalBytes += 2) : totalBytes++;
+    }
+    return totalBytes;
+  };
+
+  const isOverLimit = value => {
+    const totalBytes = countBytes(value);
+
+    return totalBytes > MAX_DESCRIPTION_BYTES ? true : false;
+  };
 
   const handleChange = ({ target }) => {
     const { value } = target;
-    countBytes(value);
     setDescription(value);
+
+    if (isOverLimit(value)) {
+      setShowsOverLimitMessage(true);
+      //TODO
+      // setReadyToUpload(false);
+    } else {
+      setShowsOverLimitMessage(false);
+      //TODO
+      // setReadyToUpload(true);
+    }
   };
 
   return (
     <textarea
       name="description"
-      className="description-uploader"
+      className={`description-uploader ${
+        showsOverLimitMessage ? 'description-over-limit' : ''
+      }`}
       placeholder="간단한 설명을 적어주세요."
       value={description}
       onChange={handleChange}
