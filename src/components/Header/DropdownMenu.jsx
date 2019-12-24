5525496f7e4db4da150ac31dd08df8a87ea51061
allenk <dev.allenk@gmail.com>
2019-12-24 23:31:15 +0900

    feat: 드롭다운메뉴에 글작성버튼 추가

--- src/components/Header/DropdownMenu.jsx
+++ src/components/Header/DropdownMenu.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './DropdownMenu.scss';
+import CommonLink from '../CommonLink/CommonLink';
 
 const DropdownMenu = ({ onClick }) => {
   return (
     <>
       <div className="drop-down-menu-wrapper" onClick={onClick} />
-      <div className="drop-down-menu">dropdown</div>
+      <div className="drop-down-menu">
+        <CommonLink to="/post/upload">
+          <div className="drop-down-menu-post-upload-btn">글 작성</div>
+        </CommonLink>
+      </div>
     </>
   );
 };
