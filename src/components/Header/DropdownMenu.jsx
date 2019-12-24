908a24633f291a7ddca6420fbc74015164db7c0e
allenk <dev.allenk@gmail.com>
2019-12-25 00:01:01 +0900

    style: 드롭다운메뉴 버튼 스타일링

--- src/components/Header/DropdownMenu.jsx
+++ src/components/Header/DropdownMenu.jsx
@@ -10,17 +10,17 @@ const DropdownMenu = ({ onClick }) => {
       <div className="drop-down-menu-wrapper" onClick={onClick} />
       <div className="drop-down-menu">
         <CommonLink to="/post/upload">
-          <div className="drop-down-menu-post-upload-btn">글 작성</div>
+          <div className="drop-down-menu-btns">글 작성</div>
         </CommonLink>
         <CommonLink to={`/profile/@${nickname}`}>
           {/* TODO: NavigationContext로 setUserId(id)필요 */}
-          <div className="drop-down-menu-post-upload-btn">내 프로필</div>
+          <div className="drop-down-menu-btns">내 프로필</div>
         </CommonLink>
         <CommonLink to="/profile/edit">
-          <div className="drop-down-menu-post-upload-btn">프로필 편집</div>
+          <div className="drop-down-menu-btns">프로필 편집</div>
         </CommonLink>
         <CommonLink to="/">
-          <div className="drop-down-menu-post-upload-btn">로그아웃</div>
+          <div className="drop-down-menu-btns">로그아웃</div>
         </CommonLink>
       </div>
     </>
