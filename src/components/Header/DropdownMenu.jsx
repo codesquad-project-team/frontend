e9e1bc198f5366890d4ba84ab1fdd0c65dd0ea1e
allenk <dev.allenk@gmail.com>
2019-12-31 00:23:48 +0900

    feat: 드롭다운메뉴에서 '내 프로필'로 이동하는 기능 추가

--- src/components/Header/DropdownMenu.jsx
+++ src/components/Header/DropdownMenu.jsx
@@ -4,7 +4,7 @@ import CommonLink from '../CommonLink/CommonLink';
 import { useLoginContext } from '../../contexts/LoginContext';
 
 const DropdownMenu = ({ onClick }) => {
-  const { nickname } = useLoginContext();
+  const { nickname, id } = useLoginContext();
   const [showsMenu, setShowsMenu] = useState(false);
 
   useEffect(() => {
@@ -18,8 +18,10 @@ const DropdownMenu = ({ onClick }) => {
         <CommonLink to="/post/upload">
           <div className="drop-down-menu-btns">글 작성</div>
         </CommonLink>
-        <CommonLink to={`/profile/@${nickname}`}>
-          {/* TODO: NavigationContext로 setUserId(id)필요 */}
+        <CommonLink
+          to={`/profile/@${nickname}`}
+          onClick={() => localStorage.setItem('targetUserId', id)}
+        >
           <div className="drop-down-menu-btns">내 프로필</div>
         </CommonLink>
         <CommonLink to="/profile/edit">
