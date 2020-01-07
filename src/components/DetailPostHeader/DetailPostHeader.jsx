68b4fb5e1928bd0ef8283af1b2acfe0945491568
allenk <dev.allenk@gmail.com>
2020-01-07 21:35:08 +0900

    feat: 포스트 삭제 기능 추가

--- src/components/DetailPostHeader/DetailPostHeader.jsx
+++ src/components/DetailPostHeader/DetailPostHeader.jsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import './DetailPostHeader.scss';
 import CommonBtn from '../CommonBtn/CommonBtn';
 import { useLoginContext } from '../../contexts/LoginContext';
+import { WEB_SERVER_URL } from '../../configs';
 
 const DetailPostHeader = ({ data }) => {
   const history = useHistory();
@@ -23,7 +24,28 @@ const DetailPostHeader = ({ data }) => {
     history.push('/post/edit');
   };
 
-  const handleDelete = () => {};
+  const handleDelete = async () => {
+    const res = await fetch(`${WEB_SERVER_URL}/post/${data.id}`, {
+      method: 'DELETE',
+      credentials: 'include'
+    });
+    console.log(res);
+    switch (res.status) {
+      case 200:
+        alert('게시글이 삭제되었습니다.');
+        history.push('/profile');
+        break;
+      case 400:
+        console.error('not exist postId');
+        break;
+      case 401:
+        console.error('unthorized');
+        break;
+      case 500:
+        alert('서버에서 문제가 생겼나봐요. 잠시 후에 다시 시도해주세요.');
+        break;
+    }
+  };
 
   return (
     <div className="detail-post-header">
