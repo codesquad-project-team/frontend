f9e9e835f65b472a869be084249f436236b2e91b
allenk <dev.allenk@gmail.com>
2020-02-11 14:15:01 +0900

    feat: 관련 포스트 눌렀을 때 해당 포스트로 이동하도록 링크 추가
    - 관련 포스트 아이템의 사진과 텍스트 사이의 공간에 링크가 걸리지 않도록 따로따로 링크 처리

--- src/components/RelatedPost/RelatedPostComment.jsx
+++ src/components/RelatedPost/RelatedPostComment.jsx
@@ -2,18 +2,28 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './RelatedPostComment.scss';
 import ProfileImage from '../ProfileImage/ProfileImage';
+import CommonLink from '../CommonLink/CommonLink';
 
 const cx = classNames.bind(styles);
 
-const RelatedPostComment = ({ companion, activity, profileImageURL }) => {
+const RelatedPostComment = ({
+  postId,
+  companion,
+  activity,
+  profileImageURL
+}) => {
   return (
     <div className={cx('wrapper')}>
-      <ProfileImage medium src={profileImageURL} />
-      <h3 className={cx('comment')}>
-        {companion}
-        <br />
-        {activity}
-      </h3>
+      <CommonLink to={`/post/${postId}`}>
+        <ProfileImage medium src={profileImageURL} />
+      </CommonLink>
+      <CommonLink to={`/post/${postId}`}>
+        <h3 className={cx('comment')}>
+          {companion}
+          <br />
+          {activity}
+        </h3>
+      </CommonLink>
     </div>
   );
 };
