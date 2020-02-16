9faec75fa4d0d25e158e2899336497bad68d5216
allenk <dev.allenk@gmail.com>
2020-02-16 16:26:55 +0900

    style: 포스트 디테일 페이지 반응형 적용
    - 캐러셀은 기본적으로 viewport size에 맞게 유동적으로 변하도록 작성
    - mobile이외의 사이즈(가로 768px 이상)에서는 너무 크지않도록 고정크기 적용

--- src/components/MapView/MapView.jsx
+++ src/components/MapView/MapView.jsx
@@ -2,18 +2,20 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { KakaoMap, Marker, CustomOverlay } from 'react-kakao-maps';
 import InfoWindow from '../MapView/InfoWindow';
+import useMediaQuerySet from '../../hooks/useMediaQuerySet';
 
 const MapView = ({
   data: { location: { latitude, longitude, ...info } = {} }
 }) => {
   const [infoDisplay, setInfoDisplay] = useState(true);
+  const { isMobile } = useMediaQuerySet();
 
   return (
     <KakaoMap
       // eslint-disable-next-line no-undef
       apiUrl={KAKAO_MAP_API_URL}
-      width="800px"
-      height="500px"
+      width={isMobile ? '95%' : '700px'}
+      height={isMobile ? '300px' : '450px'}
       level={2}
       lat={latitude}
       lng={longitude}
