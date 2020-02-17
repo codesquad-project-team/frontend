b366a0058c36e90f10f61f12bd05f2d9a949cdcc
allenk <dev.allenk@gmail.com>
2020-02-17 23:44:06 +0900

    chore: console.log 삭제

--- src/components/RelatedPost/RelatedPost.jsx
+++ src/components/RelatedPost/RelatedPost.jsx
@@ -29,7 +29,6 @@ const RelatedPost = ({ postId }) => {
   );
   const updateWindowWidth = useCallback(
     throttle(() => {
-      console.log('mobile');
       setWindowViewportWidth(window.innerWidth);
     }),
     []
