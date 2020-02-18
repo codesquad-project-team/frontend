59be731b3d33d195169fb7172600c0a60a977dff
allenk <dev.allenk@gmail.com>
2020-02-18 23:24:46 +0900

    feat: 터치에도 동작하도록 기능 추가
    - 좌표(0,0)부터 화면세로길이의 1/3지점까지의 기울기를 기준으로
     touchStart부터 touchEnd의 기울기가 기준기울기보다 크면 수직방향 동작으로 판단함

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useReducer, useEffect } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Carousel.scss';
 
@@ -9,13 +9,40 @@ const Carousel = ({ data, src, key, className, style }) => {
   const dataLength = useMemo(() => data.length, [data]);
   const isLeftEnd = !targetIndex;
   const isRightEnd = targetIndex === dataLength - 1;
+  const [touchInfo, dispatch] = useReducer(reducer, {});
 
-  const showPrev = () => setTargetIndex(index => index - 1);
-  const showNext = () => setTargetIndex(index => index + 1);
+  const showPrev = () => !isLeftEnd && setTargetIndex(index => index - 1);
+  const showNext = () => !isRightEnd && setTargetIndex(index => index + 1);
+
+  const handleTouchStart = ({ changedTouches }) => {
+    const { pageX, pageY } = changedTouches[0];
+    dispatch({ type: 'touchStart', payload: { pageX, pageY } });
+  };
+
+  const handleTouchMove = ({ changedTouches }) => {
+    const { pageX, pageY } = changedTouches[0];
+    dispatch({ type: 'touchMove', payload: { pageX, pageY } });
+  };
+
+  const handleTouchEnd = () => {
+    if (!touchInfo.isHorizontalMove) return;
+    touchInfo.direction === 'next' ? showNext() : showPrev();
+    dispatch({ type: 'touchEnd' });
+  };
+
+  useEffect(() => {
+    const criterion = window.innerHeight / 3 / window.innerWidth; //기준기울기 = (0,0)부터 화면세로길이의 1/3지점까지의 기울기
+    dispatch({ type: 'init', payload: criterion });
+  }, []);
 
   return (
     <div className={className} style={style}>
-      <div className={`${cx('viewport')}`}>
+      <div
+        className={`${cx('viewport')}`}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
         <div
           className={cx('container')}
           style={{
@@ -54,3 +81,39 @@ const Carousel = ({ data, src, key, className, style }) => {
 };
 
 export default Carousel;
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case 'init':
+      return { criterion: payload };
+    case 'touchStart':
+      return { ...state, ...payload, isTouchStarted: true };
+    case 'touchMove':
+      return { ...state, ...calculateMoveType(state, payload) };
+    case 'touchEnd':
+      return { criterion: state.criterion };
+    default:
+      //do nothing
+      break;
+  }
+};
+
+const calculateMoveType = (
+  { isTouchStarted, criterion, ...prevCoord },
+  currentCoord
+) => {
+  if (!isTouchStarted) return;
+
+  const x = Math.abs(prevCoord.pageX - currentCoord.pageX);
+  const y = Math.abs(prevCoord.pageY - currentCoord.pageY);
+  const distance = x + y;
+
+  //4px 미만으로 움직인 경우 아무동작 안함. 참고: https://d2.naver.com/helloworld/80243
+  if (distance < 4) return;
+
+  const slope = y / x;
+  const isHorizontalMove = slope < criterion;
+  const direction = prevCoord.pageX > currentCoord.pageX ? 'next' : 'prev';
+
+  return { isHorizontalMove, direction };
+};
