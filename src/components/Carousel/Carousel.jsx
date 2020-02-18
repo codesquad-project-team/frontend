53130a7e1ff2b24356004766a211321b34a160ba
allenk <dev.allenk@gmail.com>
2020-02-18 18:16:09 +0900

    feat: 버튼 클릭 시 이동하는 기능 추가

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Carousel.scss';
 
 const cx = classNames.bind(styles);
+
 const Carousel = ({ data, src, key, className, style }) => {
+  const [targetIndex, setTargetIndex] = useState(0);
+  const dataLength = useMemo(() => data.length, [data]);
+  const isLeftEnd = !targetIndex;
+  const isRightEnd = targetIndex === dataLength - 1;
+
+  const showPrev = () => setTargetIndex(index => index - 1);
+  const showNext = () => setTargetIndex(index => index + 1);
+
   return (
     <div className={`${cx('viewport')} ${className}`} style={style}>
-      <button className={cx('leftButton')}>
-        <div className={cx('leftChevron')} />
-      </button>
-      <div className={cx('container')}>
+      {!isLeftEnd && (
+        <button className={cx('leftButton')} onClick={showPrev}>
+          <div className={cx('leftChevron')} />
+        </button>
+      )}
+      <div
+        className={cx('container')}
+        style={{
+          transform: `translateX(${targetIndex * -100}%)`
+        }}
+      >
         {data.map((el, idx) => (
           <img className={cx('items')} src={el[src]} key={key || idx} />
         ))}
       </div>
-      <button className={cx('rightButton')}>
-        <div className={cx('rightChevron')} />
-      </button>
+      {!isRightEnd && (
+        <button className={cx('rightButton')} onClick={showNext}>
+          <div className={cx('rightChevron')} />
+        </button>
+      )}
     </div>
   );
 };
