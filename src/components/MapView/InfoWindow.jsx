3bc73550026bb398bc17a43f9825748bb7d020d7
allenk <dev.allenk@gmail.com>
2020-02-24 12:23:50 +0900

    fix: style import 순서에 따라 발생하는 레이아웃 버그 수정

--- src/components/MapView/InfoWindow.jsx
+++ src/components/MapView/InfoWindow.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import styles from './InfoWindow.scss';
 import CloseBtn from '../CommonBtn/CloseBtn';
+import styles from './InfoWindow.scss';
 import { IMAGE_BUCKET_URL } from '../../configs';
 
 const cx = classNames.bind(styles);
