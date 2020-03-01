c571a59f4fa75a9ed440d0781fec735c4672d983
allenk <dev.allenk@gmail.com>
2020-03-02 00:10:20 +0900

    feat: useFetch를 다른 메서드에도 사용가능하도록 기능 추가
    - 모든 api 요청을 일관된 형태로 작성할 수 있도록 기능 추가
    - useFetch가 최초 호출될 때 보내던 요청을 스킵할 수 있도록 needsJson 옵션 추가
    - useCallback삭제
     : 퍼포먼스에 큰 영향을 주지 않고, 버그의 가능성을 줄임.
      body에 전달할 값이 useFetch를 사용하는 쪽에서 전달되는 경우, 즉시 동기화될 수 있도록 useCallback 삭제.
      requestFetch의 인자로 전달받을 수도 있지만 이렇게 작성할 경우 options에 body를 작성하지 않게 되므로
      혼동을 줄 수 있다고 판단함.

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -16,44 +16,58 @@ const handleFetchError = (error, errorMap) => {
   if (typeof handler === 'function') return handler();
 
   if (!handler) {
+    console.warn(error);
     console.warn('errorMap에 정의하지 않은 에러입니다.');
     return alert('UNKNOWN_ERROR');
   }
 };
 
 /**
+ * fetch를 이용해 리소스를 요청하고 응답을 받습니다.
+ * 기본적으로 URL이 변경될 때마다 요청을 보냅니다.
  * 모든 인자는 객체의 프로퍼티로 전달해야 합니다.
- * {URL, options, callback, errorMap}
- * @param {string} URL fetch요청할 URL
- * @param {Object} options fetch함수의 두번째 인자로 전달할 옵션객체
- * @param {Function} callback fetch받은 data를 상태로 저장할 setState함수
- * @returns {Object} {data: json data, loading: {boolean}, refetch: {function}}
+ * @param {{URL: string, options: Object, callback: Function, needsJson: boolean}} object
+ * @property {string} URL - fetch요청할 URL
+ * @property {Object} options - fetch함수의 두번째 인자로 전달할 옵션객체
+ * @property {Function} callback - response status가 200번대인 경우 실행할 콜백함수(204제외).
+ *   needsJson이 true인 경우 json객체를 인자로 전달받습니다.
+ * @property {Object} errorMap - fetch과정에서 발생하는 에러를 핸들링할 함수를 매핑한 객체.
+ * @property {boolean} needsJson - response.json()이 필요없는 경우 false. 기본값은 true.
+ *   이 값이 false이면 useFetch가 호출될 때 자동으로 fetch를 실행하지 않습니다.
+ *   useFetch()에서 리턴되는 requestFetch함수를 이용해 fetch를 실행할 수 있습니다.
+ * @returns {Object} {data: (json data), loading: (Boolean), requestFetch: (Function)}
  */
-const useFetch = ({ URL, options = {}, callback, errorMap }) => {
+const useFetch = ({ URL, options, callback, errorMap, needsJson = true }) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  const fetchData = useCallback(async () => {
+  const requestFetch = async () => {
     try {
       setLoading(true);
       const res = await fetch(URL, options);
       if (res instanceof Promise) throw Error('REQUEST FAILED');
       if (!res.ok) throw Error(res.status);
       if (res.status === 204) throw Error(res.status); //request success but no content
-      const json = await res.json();
-      callback ? callback(json) : setData(json);
+
+      if (needsJson) {
+        const json = await res.json();
+        callback ? callback(json) : setData(json);
+      } else {
+        callback && callback();
+      }
     } catch (error) {
       handleFetchError(error, errorMap);
     } finally {
       setLoading(false);
     }
-  }, [URL]);
+  };
 
   useEffect(() => {
-    fetchData();
+    if (!needsJson) return;
+    requestFetch();
   }, [URL]);
 
-  return { data, loading, refetch: fetchData };
+  return { data, loading, requestFetch };
 };
 
 export default useFetch;
