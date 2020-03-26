2e9b3ad189d6758138e47108cacf422fa0d956cb
allenk <dev.allenk@gmail.com>
2020-03-26 12:04:52 +0900

    chore: useFetch 주석 및 일관성을 위한 if구문 수정

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -2,7 +2,8 @@ import { useState, useEffect, useMemo } from 'react';
 
 /**
  * fetch를 이용해 리소스를 요청하고 응답을 받습니다.
- * @param {{onRequest: Function, onSuccess: Function, onError: Object, watch: any, loadStatus: boolean}} object
+ * @param {{onRequest: Function, onSuccess: Function, onError: Object|Function,
+            watch: string|number|array, autoFetch: boolean, loadStatus: boolean}} object
  * @property {Function} onRequest - 요청을 보내기위해 실행할 함수. 프로미스를 리턴하는 함수여야 합니다.
  *   useFetch가 리턴하는 request 함수에 전달된 인자는 onRequest에 전달됩니다.
  * @property {Function} [onSuccess] - response status가 200번대인 경우 실행할 콜백함수(204제외).
@@ -11,7 +12,7 @@ import { useState, useEffect, useMemo } from 'react';
  * @property {Object|Function} [onError] - 에러를 핸들링할 함수 또는 함수를 매핑한 객체.
  *   status code에 따라 실행될 함수를 매핑합니다. 매핑된 값이 string인 경우 alert를 실행합니다.
  *   error객체를 리턴받고 싶은 경우 이 값에 아무것도 전달하지 않을 수 있습니다.
- * @property {any} [watch] - 주어진 값이 변경되는 경우 요청을 보냅니다.
+ * @property {string|number|array} [watch] - 주어진 값이 변경되는 경우 요청을 보냅니다.
  *   이 값이 true인 경우 컴포넌트가 처음 렌더링 될 때 onRequest함수를 실행합니다.
  * @property {boolean} [autoFetch] - 이 값이 true인 경우 컴포넌트가 처음 렌더링 될 때 요청을 보냅니다.
  *   watch할 값이 없는 경우 사용하는 용도입니다. watch 옵션이 false가 아닌 경우 이 옵션은 무시됩니다.
@@ -49,7 +50,7 @@ const useFetch = ({
 
   const request = async param => {
     try {
-      loadStatus && setLoading(true);
+      if (loadStatus) setLoading(true);
       const res = await onRequest(param);
 
       if (res instanceof Promise) throw Error('REQUEST FAILED'); //서버로부터 응답 자체를 받지 못한 경우.
@@ -66,7 +67,7 @@ const useFetch = ({
       if (isFunction(onError)) onError();
       else handleFetchError(error, onError);
     } finally {
-      loadStatus && setLoading(false);
+      if (loadStatus) setLoading(false);
     }
   };
 
